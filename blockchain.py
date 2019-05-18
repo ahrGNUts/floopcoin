1a662bc24aa8bfd4f667181a6c274654f96e2377
Patrick <revmydev@gmail.com>
2019-05-17 17:50:45 -0700

    set default arg value for add_value

--- blockchain.py
+++ blockchain.py
@@ -1,14 +1,14 @@
-blockchain = [[1]]
+blockchain = []
 
 def get_last_blockchain_value():
     return blockchain[-1]
 
-def add_value(transaction_amount):
-    blockchain.append([get_last_blockchain_value(), transaction_amount])
+def add_value(transaction_amount, last_transaction=[1]):
+    blockchain.append([last_transaction, transaction_amount])
 
 
 add_value(5.4)
-add_value(9.8)
-add_value(92)
+add_value(9.8, get_last_blockchain_value())
+add_value(92, get_last_blockchain_value())
 
 print(blockchain)
