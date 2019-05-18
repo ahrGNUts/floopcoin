79b9b8e6b7f3e81f8a6b7e36a4a20fa95659b0c1
Patrick <revmydev@gmail.com>
2019-05-17 17:42:09 -0700

    added a getter for last blockchain value

--- blockchain.py
+++ blockchain.py
@@ -1,10 +1,14 @@
 blockchain = [[1]]
 
+def get_last_blockchain_value():
+    return blockchain[-1]
+
 def add_value(transaction_amount):
-    blockchain.append([blockchain[-1], transaction_amount])
-    print(blockchain)
+    blockchain.append([get_last_blockchain_value(), transaction_amount])
 
 
 add_value(5.4)
 add_value(9.8)
 add_value(92)
+
+print(blockchain)
