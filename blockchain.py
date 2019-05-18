586853672fc58d3026db476924099d249d1b2c86
Patrick <revmydev@gmail.com>
2019-05-17 17:56:25 -0700

    interactivity with input()

--- blockchain.py
+++ blockchain.py
@@ -7,8 +7,13 @@ def add_value(transaction_amount, last_transaction=[1]):
     blockchain.append([last_transaction, transaction_amount])
 
 
-add_value(5.4)
-add_value(last_transaction=get_last_blockchain_value(), transaction_amount=9.8)
-add_value(92, get_last_blockchain_value())
+tx_amt = float(input('please enter your transaction amount: '))
+add_value(tx_amt)
+
+tx_amt = float(input('please enter your transaction amount: '))
+add_value(last_transaction=get_last_blockchain_value(), transaction_amount=tx_amt)
+
+tx_amt = float(input('please enter your transaction amount: '))
+add_value(tx_amt, get_last_blockchain_value())
 
 print(blockchain)
