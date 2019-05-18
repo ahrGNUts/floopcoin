e0b3051b5be502667d32ca9a2986c3fb4e0998ee
Patrick <revmydev@gmail.com>
2019-05-17 18:16:17 -0700

    cleaning things up with loops, more functions, if statements

--- blockchain.py
+++ blockchain.py
@@ -6,16 +6,29 @@ def get_last_blockchain_value():
 def add_value(transaction_amount, last_transaction=[1]):
     blockchain.append([last_transaction, transaction_amount])
 
-def get_user_input():
+def get_transaction_value():
     return float(input('please enter your transaction amount: '))
 
-tx_amt = get_user_input()
+def get_user_choice():
+    return input('Your choice: ')
+
+def print_blockchain_elements():
+    for block in blockchain:
+        print('Outputting blocks:')
+        print(block)
+
+
+tx_amt = get_transaction_value()
 add_value(tx_amt)
 
-tx_amt = get_user_input()
-add_value(last_transaction=get_last_blockchain_value(), transaction_amount=tx_amt)
+while True:
+    print('Make a selection: ')
+    print('1. Add new transaction value')
+    print('2. Output the blockchain blocks')
+    choice = get_user_choice()
 
-tx_amt = get_user_input()
-add_value(tx_amt, get_last_blockchain_value())
-
-print(blockchain)
+    if choice == '1':
+        tx_amt = get_transaction_value()
+        add_value(tx_amt, get_last_blockchain_value())
+    elif choice == '2':
+        print_blockchain_elements()
