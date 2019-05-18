421e8de4a3dec81b7a6541b2b6791b325fa02af9
Patrick <revmydev@gmail.com>
2019-05-17 17:53:04 -0700

    kwargs demo

--- blockchain.py
+++ blockchain.py
@@ -8,7 +8,7 @@ def add_value(transaction_amount, last_transaction=[1]):
 
 
 add_value(5.4)
-add_value(9.8, get_last_blockchain_value())
+add_value(last_transaction=get_last_blockchain_value(), transaction_amount=9.8)
 add_value(92, get_last_blockchain_value())
 
 print(blockchain)
