ec79536758b97fb97d90d4d9e85b96557aad47e8
Patrick <revmydev@gmail.com>
2019-05-22 15:39:14 -0700

    improved blockchain validation logic

--- blockchain.py
+++ blockchain.py
@@ -1,11 +1,15 @@
 genesis_block = {
-    'previousHash': '',
+    'previous_hash': '',
     'index': 0,
     'transactions': []
 }
 blockchain = [genesis_block]
 open_transactions = []
 owner = 'Me'
+
+
+def hash_block(block):
+    return '-'.join([str(block[key]) for key in block])
 
 def get_last_blockchain_value():
     if len(blockchain) < 1:
@@ -22,14 +26,11 @@ def add_transaction(recipient, sender=owner, amount=1.0):
     open_transactions.append(transaction)
 
 
-
 def mine_block():
     last_block = blockchain[-1]
-    hashed_block = '-'.join([str(last_block[key]) for key in last_block])
-
-    print(hashed_block)
+    hashed_block = hash_block(last_block)
     block = {
-        'previousHash': hashed_block,
+        'previous_hash': hashed_block,
         'index': len(blockchain),
         'transactions': open_transactions
     }
@@ -52,20 +53,12 @@ def print_blockchain_elements():
         print('-' * 20)
 
 def verify_chain():
-    is_valid = True
-
-    for block_index in range(len(blockchain)):
-        if block_index == 0:
+    for (index, block) in enumerate(blockchain):
+        if index == 0:
             continue
-        elif blockchain[block_index][0] == blockchain[block_index - 1]:
-            is_valid = True
-        else:
-            is_valid = False
-            break
-        block_index += 11
-
-    return is_valid
-
+        if block['previous_hash'] != hash_block(blockchain[index - 1]):
+            return False
+    return True
 
 waiting_for_input = True;
 
@@ -91,12 +84,16 @@ while waiting_for_input:
         waiting_for_input = False
     elif choice == 'h':
         if len(blockchain) > 0:
-            blockchain[0] = [2]
+            blockchain[0] = {
+                'previous_hash': '',
+                'index': 0,
+                'transactions': [{'sender': 'Chris', 'recipient': 'Me', 'amount': 100}]
+            }
     else:
         print('Invalid. Please make a selection from the list.')
 
-    # if not verify_chain():
-    #     print('Invalid blockchain')
-    #     break
+    if not verify_chain():
+        print('Invalid blockchain')
+        break
 
 print('Done.')
